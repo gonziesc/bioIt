0d048519308f01ccada01fe22ffaf1122706447d
Gonzalo Escandarani <gonziesc@gmail.com>
2019-09-29 18:53:05 -0300

    adding first script

--- readME.md
+++ readME.md
@@ -4,4 +4,6 @@
 
 - We chose [Celiac disease](https://www.omim.org/entry/212750?search=celiac&highlight=(celiac|coeliac))
 - Check the [gen](https://www.ncbi.nlm.nih.gov/gene/3117/)
-- Select the [nucleotide](https://www.ncbi.nlm.nih.gov/nuccore/NC_000006.12?report=genbank&from=32637406&to=32654846) 
+- Select the [nucleotide](https://www.ncbi.nlm.nih.gov/nuccore/NC_000006.12?report=genbank&from=32637406&to=32654846)
+- Install perl and [BioPerl::SeqIO](https://bioperl.org/howtos/Beginners_HOWTO.html) (we used `cpanm Bio::SeqIO`)
+- Run in terminal `perl ./sequence\ processing/seqProcessing.pm`
---
+++ sequence processing/seqProcessing.pm
@@ -0,0 +1,35 @@
+# first, bring in the SeqIO module
+use Bio::SeqIO;
+
+# Notice that you do not have to use any Bio:SeqI
+# objects, because SeqIO does this for you. In fact, it
+# even knows which SeqI object to use for the provided format.
+
+# Bring in the file and format, or die with a nice
+# usage statement if one or both arguments are missing.
+my $usage = "getaccs.pl file format "; 
+my $file = "./sequence processing/sequence.genbank"; 
+my $format = "genbank";
+
+# Now create a new SeqIO object to bring in the input
+# file. The new method takes arguments in the format
+# key => value, key => value. The basic keys that it
+# can accept values for are '-file' which expects some
+# information on how to access your data, and '-format'
+# which expects one of the Bioperl-format-labels mentioned
+# above. Although it is optional, it is good
+# programming practice to provide > and &lt; in front of any
+# filenames provided in the -file parameter. This makes the
+# resulting filehandle created by SeqIO explicitly read (&lt;)
+# or write(>). It will definitely help others reading your
+# code understand the function of the SeqIO object.
+
+my $inseq = Bio::SeqIO->new(-file   => "$file",
+                            -format => $format, );
+
+# Now that we have a seq stream,
+# we need to tell it to give us a $seq.
+#  We do this using the 'next_seq' method of SeqIO.
+while (my $seq = $inseq->next_seq) {
+   print $seq->accession_number,"\n";
+}
